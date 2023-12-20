b6dfe5d8d00f5392e61f4719223a9b8f28ebc478
Jay-Sparks <jay_sparks@icloud.com>
2023-12-20 14:42:48 +0000

    resolving pointer issue in components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,9 +40,9 @@ const Home = () => {
                 <strong>Unleash your imagination</strong>{" "} and define your own style!
               </p>
               <CustomButton 
-                type="filled"
-                title="Customize it"
-                handleClick={() => state.intro = false}
+                type="filled" 
+                title="Customize it" 
+                handleClick={() => state.intro = false} 
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
             </motion.div>
